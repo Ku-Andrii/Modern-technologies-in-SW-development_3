a0510bb4fd378148491a95b13076a9738951b779
Andrii Kutsenko <79030234+Ku-Andrii@users.noreply.github.com>
2021-05-07 15:10:52 +0300

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,8 +11,7 @@ namespace UnitTestMTD_Laba_3
         public void TestForWriteNumber()
         {
             var l1 = Singleton.GetInstance();
-            l1.WriteNumber(27);
-            var actual = l1.Value;
+            var actual = l1.WriteNumber(25);
             var expected = " 27";
             Assert.AreEqual(expected, actual);
         }
@@ -23,6 +22,15 @@ namespace UnitTestMTD_Laba_3
             var l2 = Singleton.GetInstance();
             var actual = l1;
             var expected = l2;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestForMultithreading()
+        {
+            var l1 = Singleton.GetInstance();
+            var l2 = Singleton.GetInstance();
+            var actual = l1.WriteNumber(27);
+            var expected = l2.WriteNumber(18);
             Assert.AreEqual(expected, actual);
         }
     }
